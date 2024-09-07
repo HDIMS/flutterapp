d7223785ebc110cc4cfe161109170be8a5d13cb5
Narmadha J S <138014247+narmadhajs@users.noreply.github.com>
2024-09-07 21:36:38 +0530

    Create login_form.dart

---
+++ lib/pages/login_form.dart
@@ -0,0 +1,406 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+
+//import '../../components/background.dart';
+//import '../responsive.dart';
+//import 'components/login_signup_btn.dart';
+//import 'components/welcome_image.dart';
+//import '../../../components/already_have_an_account_acheck.dart';
+//import '../../constants.dart';
+//import '../../Signup/signup_screen.dart';
+// import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+// import 'package:flutter/material.dart';
+//import 'package:flutterapp/pages/Screens/responsive.dart';
+//import '../../components/background.dart';
+//import 'components/login_form.dart';
+//import 'components/login_screen_top_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+
+class LoginForm extends StatelessWidget {
+  const LoginForm({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            textInputAction: TextInputAction.next,
+            cursorColor: kPrimaryColor,
+            onSaved: (email) {},
+            decoration: const InputDecoration(
+              hintText: "Your email",
+              prefixIcon: Padding(
+                padding: EdgeInsets.all(defaultPadding),
+                child: Icon(Icons.person),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: defaultPadding),
+            child: TextFormField(
+              textInputAction: TextInputAction.done,
+              obscureText: true,
+              cursorColor: kPrimaryColor,
+              decoration: const InputDecoration(
+                hintText: "Your password",
+                prefixIcon: Padding(
+                  padding: EdgeInsets.all(defaultPadding),
+                  child: Icon(Icons.lock),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: defaultPadding),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text(
+              "Login".toUpperCase(),
+            ),
+          ),
+          // const SizedBox(height: defaultPadding),
+          // AlreadyHaveAnAccountCheck(
+          //   press: () {
+          //     Navigator.push(
+          //       context,
+          //       MaterialPageRoute(
+          //         builder: (context) {
+          //           return const SignUpScreen();
+          //         },
+          //       ),
+          //     );
+          //   },
+          // ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class LoginScreenTopImage extends StatelessWidget {
+  const LoginScreenTopImage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Text(
+          "LOGIN",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(height: defaultPadding * 2),
+        Row(
+          children: [
+            const Spacer(),
+            Expanded(
+              flex: 8,
+              child: SvgPicture.asset("assets/icons/login.svg"),
+            ),
+            const Spacer(),
+          ],
+        ),
+        const SizedBox(height: defaultPadding * 2),
+      ],
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Background(
+      child: SingleChildScrollView(
+        child: Responsive(
+          mobile: MobileLoginScreen(),
+          desktop: Row(
+            children: [
+              Expanded(
+                child: LoginScreenTopImage(),
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 450,
+                      child: LoginForm(),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MobileLoginScreen extends StatelessWidget {
+  const MobileLoginScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        LoginScreenTopImage(),
+        Row(
+          children: [
+            Spacer(),
+            Expanded(
+              flex: 8,
+              child: LoginForm(),
+            ),
+            Spacer(),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+
+class Background extends StatelessWidget {
+  final Widget child;
+  const Background({
+    Key? key,
+    required this.child,
+    this.topImage = "assets/images/main_top.png",
+    this.bottomImage = "assets/images/login_bottom.png",
+  }) : super(key: key);
+
+  final String topImage, bottomImage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: SizedBox(
+        width: double.infinity,
+        height: MediaQuery.of(context).size.height,
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Positioned(
+              top: 0,
+              left: 0,
+              child: Image.asset(
+                topImage,
+                width: 120,
+              ),
+            ),
+            // Positioned(
+            //   bottom: 0,
+            //   right: 0,
+            //   child: Image.asset(bottomImage, width: 120),
+            // ),
+            SafeArea(child: child),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class WelcomeImage extends StatelessWidget {
+  const WelcomeImage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Text(
+          "WELCOME BACK!!!",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(height: defaultPadding * 2),
+        Row(
+          children: [
+            const Spacer(),
+            Expanded(
+              flex: 8,
+              child: SvgPicture.asset(
+                "assets/icons/chat.svg",
+              ),
+            ),
+            const Spacer(),
+          ],
+        ),
+        const SizedBox(height: defaultPadding * 2),
+      ],
+    );
+  }
+}
+// import 'package:flutter/material.dart';
+
+class Responsive extends StatelessWidget {
+  final Widget mobile;
+  final Widget? tablet;
+  final Widget desktop;
+
+  const Responsive({
+    Key? key,
+    required this.mobile,
+    this.tablet,
+    required this.desktop,
+  }) : super(key: key);
+
+  static bool isMobile(BuildContext context) =>
+      MediaQuery.of(context).size.width < 576;
+
+  static bool isTablet(BuildContext context) =>
+      MediaQuery.of(context).size.width >= 576 &&
+      MediaQuery.of(context).size.width <= 992;
+
+  static bool isDesktop(BuildContext context) =>
+      MediaQuery.of(context).size.width > 992;
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    if (size.width > 992) {
+      return desktop;
+    } else if (size.width >= 576 && tablet != null) {
+      return tablet!;
+    } else {
+      return mobile;
+    }
+  }
+}
+
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Background(
+      child: SingleChildScrollView(
+        child: SafeArea(
+          child: Responsive(
+            desktop: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Expanded(
+                  child: WelcomeImage(),
+                ),
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: 450,
+                        child: LoginAndSignupBtn(),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            mobile: MobileWelcomeScreen(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MobileWelcomeScreen extends StatelessWidget {
+  const MobileWelcomeScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        WelcomeImage(),
+        Row(
+          children: [
+            Spacer(),
+            Expanded(
+              flex: 8,
+              child: LoginAndSignupBtn(),
+            ),
+            Spacer(),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+//import '../../Signup/signup_screen.dart';
+
+class LoginAndSignupBtn extends StatelessWidget {
+  const LoginAndSignupBtn({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) {
+                  return const LoginScreen();
+                },
+              ),
+            );
+          },
+          child: Text(
+            "Login".toUpperCase(),
+          ),
+        ),
+        const SizedBox(height: 16),
+        // ElevatedButton(
+        //   onPressed: () {
+        //     Navigator.push(
+        //       context,
+        //       MaterialPageRoute(
+        //         builder: (context) {
+        //           return const SignUpScreen();
+        //         },
+        //       ),
+        //     );
+        //   },
+        //   style: ElevatedButton.styleFrom(
+        //     backgroundColor: kPrimaryLightColor,
+        //     elevation: 0,
+        //   ),
+        //   child: Text(
+        //    "Sign Up".toUpperCase(),
+        //    style: const TextStyle(color: Colors.black),
+        //  ),
+     // ),
+      ],
+    );
+  }
+}
+//import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF6F35A5);
+const kPrimaryLightColor = Color(0xFFF1E6FF);
+
+const double defaultPadding = 16.0;
