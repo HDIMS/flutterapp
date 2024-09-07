cd933c9b5d89087ca962bfc903a0a66820f28710
Narmadha J S <138014247+narmadhajs@users.noreply.github.com>
2024-09-07 18:27:52 +0530

    Update login_screen.dart

--- lib/pages/Screens/Login/login_screen.dart
+++ lib/pages/Screens/Login/login_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_auth/responsive.dart';
+import 'package:flutterapp/responsive.dart';
 
 import '../../components/background.dart';
 import 'components/login_form.dart';
